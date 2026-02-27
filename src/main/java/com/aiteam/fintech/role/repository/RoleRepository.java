491518e6a92e559ce9164628b4a2d1346500c313
Aiteam55 <mitiaandria1@gmail.com>
2026-02-27 18:34:57 +0300

    add role repository

---
+++ src/main/java/com/aiteam/fintech/role/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.aiteam.fintech.role.repository;
+
+import com.aiteam.fintech.role.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
